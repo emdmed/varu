6a460a390f5e97c6d4dbc2d9b8d20231cb5a0f68
enrique <enrique.darderes@gmail.com>
2025-11-03 20:20:05 +0100

    updates

--- src/components/project.js
+++ src/components/project.js
@@ -3,7 +3,6 @@ import { Box, Text } from "ink";
 import Spinner from "ink-spinner";
 
 const Project = ({ scrollOffset, index, selectedIndex, runningProcesses, nodeModulesSizes, project }) => {
-
   const actualIndex = scrollOffset + index;
   const isSelected = actualIndex === selectedIndex;
   const processInfo = runningProcesses[project.path];
@@ -11,8 +10,16 @@ const Project = ({ scrollOffset, index, selectedIndex, runningProcesses, nodeMod
   const isRunning = processInfo && processInfo.hasDevServer
   const hasEditor = processInfo && processInfo.hasEditor
 
+  const borderStyle = isSelected ? "round" : "single"
+  const borderColor = isSelected ? undefined : "gray"
+
   return (
-    <Box justifyContent="space-between" borderStyle={isSelected ? "round" : ""} key={actualIndex} >
+    <Box
+      justifyContent="space-between"
+      borderStyle={borderStyle}
+      borderColor={borderColor}
+      key={actualIndex}
+    >
       <Box gap={1}>
         <Text inverse={isSelected || isRunning} bold color={isRunning ? "green" : "white"} >
           {" "} {project.projectName} {isRunning ? <Spinner type="triangle" /> : null} {" "}
@@ -20,16 +27,14 @@ const Project = ({ scrollOffset, index, selectedIndex, runningProcesses, nodeMod
         <Text color="gray">
           ({project.framework})
         </Text>
-
         {modulesInfo && modulesInfo.exists && (
-          <Text color="magenta">deps {modulesInfo.sizeFormatted}</Text>
+          <Text color="magenta">Modules {modulesInfo.sizeFormatted}</Text>
         )}
         {!modulesInfo && <Text color="magenta"><Spinner /></Text>}
       </Box>
       <Box gap={1}>
         {hasEditor && (
           <Text color="cyan">{" "}[vim]{" "}</Text>
-
         )}
         {project.gitBranch && (
           <Text color="yellow">[{project.gitBranch}]</Text>
